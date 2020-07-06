af78db7d287f9bffa9798e696ab1c21da2cb48d0
James Yin <ywhjames@hotmail.com>
2020-07-06 10:53:31 +0800

    refactor(utf8): simplify loop

--- src/string/utf8.c
+++ src/string/utf8.c
@@ -41,48 +41,17 @@ size_t utf8_word_len(const char* s, size_t slen) {
 
 /**
  * utf8_word_position - 取得utf8编码字符串的字位置
- * @param s - string buffer
- * @param wp - the size of buffer wp must bigger than slen
- * @param slen - length of string in buffer s
+ * @param in_s - string buffer
+ * @param in_slen - length of string in buffer s
+ * @param out_wp - the size of buffer wp must bigger than slen
  * @return
  */
 int utf8_word_pos(const char* in_s, size_t in_slen, size_t* out_wp) {
   size_t wlen = 0;
   size_t i = 0;
   while (i < in_slen) {
-    switch (UTF8_WORD_SIZE(in_s + i)) {
-      case 6:
-        if (i >= in_slen) {
-          break;
-        }
-        out_wp[i++] = wlen;
-      case 5:
-        if (i >= in_slen) {
-          break;
-        }
-        out_wp[i++] = wlen;
-      case 4:
-        if (i >= in_slen) {
-          break;
-        }
-        out_wp[i++] = wlen;
-      case 3:
-        if (i >= in_slen) {
-          break;
-        }
-        out_wp[i++] = wlen;
-      case 2:
-        if (i >= in_slen) {
-          break;
-        }
-        out_wp[i++] = wlen;
-      case 1:
-        if (i >= in_slen) {
-          break;
-        }
-        out_wp[i++] = wlen;
-      default:
-        break;
+    for (size_t n = UTF8_WORD_SIZE(in_s + i); n > 0 && i < in_slen; n--) {
+      out_wp[i++] = wlen;
     }
     wlen++;
   }
